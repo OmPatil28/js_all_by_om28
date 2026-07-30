3c0e797b42fc7b5a6478f86a9c19ed3f20f38d67
OmPatil28 <kishorpatil939011@gmail.com>
2026-07-30 19:49:15 +0000

    Update JavaScript basics

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+// let score = "123abs"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInumber = Number(score);
+
+// console.log(typeof valueInumber);
+
+// console.log(valueInumber);
+
+
+let name = 1;
+
+let booleanIsname = Boolean(name);
+
+console.log(booleanIsname);
