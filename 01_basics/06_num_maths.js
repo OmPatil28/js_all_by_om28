43e30af76891de1199b6b0801e8be6d4586a403c
OmPatil28 <kishorpatil939011@gmail.com>
2026-08-03 15:07:01 +0000

    Add 01_basics/06_num_maths.js

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 123.998877
+// console.log(otherNumber.toPrecision(5));
+
+// const hundred = 1000000;
+// console.log(hundred.toLocaleString('en-In'));
+
+
+// +++++++++++++++++++ MAths+++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(3, 5, 1, 9));
+// console.log(Math.max(3, 5, 8, 9));
+
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
