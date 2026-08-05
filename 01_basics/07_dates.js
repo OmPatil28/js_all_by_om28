8a72e9277c88c9ff2ffb41daf063e6bc149595bb
OmPatil28 <kishorpatil939011@gmail.com>
2026-08-05 07:44:21 +0000

    Add 07_dates.js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+// dates
+
+// let date = new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+
+let myCreatedDate = new Date("28/04/2005")
+// console.log(myCreatedDate.toLocaleString());
+
+let mytimeStamp = Date.now();
+// console.log(mytimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let NewDate = new Date();
+console.log(NewDate);
+console.log(NewDate.getMonth());
+console.log(NewDate.getDay());
